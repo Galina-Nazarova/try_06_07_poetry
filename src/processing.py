bad9aff88214fb15e585c25c869f9c3dbb9ac3c5
Galina N <silko_gy@mail.ru>
2024-07-24 19:50:27 +0700

    add sort_by_date prepared

--- src/processing.py
+++ src/processing.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 
 def filter_by_state(dict_list:list, state_key='EXECUTED') -> list:
     """ Функция принимает список словарей и опционально значение для ключа state
@@ -13,8 +15,7 @@ def sort_by_date(dict_list:list, sort_rules='True') -> list:
     """Функция принимает список словарей и необязательный параметр,
     задающий порядок сортировки (по умолчанию — убывание).
     Функция должна возвращать новый список, отсортированный по дате (date)."""
-
-    return sorted_dict_list
+    return sorted(data, key=lambda x: x['date'],reverse=bool(sort_rules))
 
 if __name__ == '__main__':
     data = [
@@ -24,17 +25,4 @@ if __name__ == '__main__':
         {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
     ]
     print(filter_by_state(data, 'CANCELED'))
-    data_2 = [
-        {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
-    ]
     print(sort_by_date(data, 'True'))
-    """# Выход функции со статусом по умолчанию 'EXECUTED'
-    [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-     {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]
-
-    # Выход функции, если вторым аргументов передано 'CANCELED'
-    [{'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-     {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]"""
